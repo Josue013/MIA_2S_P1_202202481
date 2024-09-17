b62b8a486b27599b4e60ce225763d5e5bfcaeef3
Josue013 <josuepinto013@gmail.com>
2024-09-17 17:15:42 -0600

    cambio en analizar Mkdisk

--- Backend/filesystem/Analizador.go
+++ Backend/filesystem/Analizador.go
@@ -190,128 +190,125 @@ func AnalizarComando(comando string) string {
 
 // AnalizarMkdisk recibe un comando mkdisk y lo analiza
 func AnalizarMkdisk(comando *[]string) string {
-	//mkdisk -unit=M -path="/home 1/mis discos/Disco3.mia"
-	//0 		1     2     3"/home/mis     4         5discos/Disco3.mia"
+	// mkdisk -unit=M -path="/home 1/mis discos/Disco3.mia"
+	// 0 		1     2     3"/home/mis     4         5discos/Disco3.mia"
 	var respuesta string
-	//Eliminar el primer valor del comando
+	// Eliminar el primer valor del comando
 	*comando = (*comando)[1:]
-	//-size=5 -unit=M -path="/home/mis discos/Disco3.mia"
-	//Booleanos para saber si se encontro el size, unit, fit, path
+	// -size=5 -unit=M -path="/home/mis discos/Disco3.mia"
+	// Booleanos para saber si se encontró el size, unit, fit, path
 	var size, unit, path, fit bool
-	//Variables para guardar el valor del size, unit, fit, path
+	// Variables para guardar el valor del size, unit, fit, path
 	var valorSize, valorUnit, valorFit, valorPath string
-	//Iterar sobre el comando
+	// Iterar sobre el comando
 	valorFit = "f"
 	valorUnit = "m"
 	for _, valor := range *comando {
-		bandera := obtenerBandera(valor)
-		banderaValor := obtenerValor(valor)
-		if bandera == "-size" {
-			size = true
-			valorSize = banderaValor
-			*comando = (*comando)[1:]
-		} else if bandera == "-unit" {
-			unit = true
-			valorUnit = banderaValor
-			valorUnit = strings.ToLower(valorUnit)
-			*comando = (*comando)[1:]
-		} else if bandera == "-fit" {
-			fit = true
-			valorFit = banderaValor
-			valorFit = strings.ToLower(valorFit)
-			*comando = (*comando)[1:]
-		} else if bandera == "-path" {
-			path = true
-			// Verificar si el path tiene comillas
-			// -path="/home 1/mis discos/Disco3.mia"
-			if strings.Contains(banderaValor, "\"") {
-				// Eliminar las comillas del inicio
-				banderaValor = strings.Replace(banderaValor, "\"", "", -1)
-				// Inicializar valorPath con el valor actual
-				valorPath = banderaValor
-				// Eliminar el primer valor del comandoSeparado
-				*comando = (*comando)[1:]
-				// Iterar sobre el comando
-				Contador := 0
-				for _, valor := range *comando {
-					// Si el valor contiene comillas
-					if strings.Contains(valor, "\"") {
-						// Eliminar las comillas del final
-						valor = strings.Replace(valor, "\"", "", -1)
-						// Agregar el valor al path
-						valorPath += " " + valor
-						Contador++
-						break
+			bandera := obtenerBandera(valor)
+			banderaValor := obtenerValor(valor)
+			if bandera == "-size" {
+					size = true
+					valorSize = banderaValor
+			} else if bandera == "-unit" {
+					unit = true
+					valorUnit = banderaValor
+					valorUnit = strings.ToLower(valorUnit)
+			} else if bandera == "-fit" {
+					fit = true
+					valorFit = banderaValor
+					valorFit = strings.ToLower(valorFit)
+			} else if bandera == "-path" {
+					path = true
+					// Verificar si el path tiene comillas
+					// -path="/home 1/mis discos/Disco3.mia"
+					if strings.Contains(banderaValor, "\"") {
+							// Eliminar las comillas del inicio
+							banderaValor = strings.Replace(banderaValor, "\"", "", -1)
+							// Inicializar valorPath con el valor actual
+							valorPath = banderaValor
+							// Eliminar el primer valor del comandoSeparado
+							*comando = (*comando)[1:]
+							// Iterar sobre el comando
+							Contador := 0
+							for _, valor := range *comando {
+									// Si el valor contiene comillas
+									if strings.Contains(valor, "\"") {
+											// Eliminar las comillas del final
+											valor = strings.Replace(valor, "\"", "", -1)
+											// Agregar el valor al path
+											valorPath += " " + valor
+											Contador++
+											break
+									} else {
+											// Agregar el valor al path
+											valorPath += " " + valor
+											Contador++
+									}
+							}
+							// Eliminar los valores del comando
+							*comando = (*comando)[Contador:]
 					} else {
-						// Agregar el valor al path
-						valorPath += " " + valor
-						Contador++
+							valorPath = banderaValor
 					}
-				}
-				// Eliminar los valores del comando
-				*comando = (*comando)[Contador:]
 			} else {
-				valorPath = banderaValor
-				*comando = (*comando)[1:]
+					fmt.Println("Error: Parámetro no reconocido")
+					respuesta += "Error: Parámetro no reconocido\n"
+					return respuesta
 			}
-		} else {
-			fmt.Println("Error: Parametro no reconocida")
-			respuesta += "Error: Parametro no reconocida\n"
-		}
-
 	}
+
 	if !size {
-		fmt.Println("Error: Falta el parametro size")
-		respuesta += "Error: Falta el parametro size\n"
-		return respuesta
+			fmt.Println("Error: Falta el parámetro size")
+			respuesta += "Error: Falta el parámetro size\n"
+			return respuesta
 	} else if !path {
-		fmt.Println("Error: Falta el parametro path")
-		respuesta += "Error: Falta el parametro path\n"
-		return respuesta
+			fmt.Println("Error: Falta el parámetro path")
+			respuesta += "Error: Falta el parámetro path\n"
+			return respuesta
 	} else {
-		if fit {
-			if valorFit != "bf" && valorFit != "ff" && valorFit != "wf" {
-				fmt.Println("Error: Fit no reconocido")
-				respuesta += "Error: Fit no reconocido\n"
-				return respuesta
-			} else {
-				if valorFit == "bf" {
-					valorFit = "b"
-				} else if valorFit == "ff" {
-					valorFit = "f"
-				} else if valorFit == "wf" {
-					valorFit = "w"
-				}
+			if fit {
+					if valorFit != "bf" && valorFit != "ff" && valorFit != "wf" {
+							fmt.Println("Error: Fit no reconocido")
+							respuesta += "Error: Fit no reconocido\n"
+							return respuesta
+					} else {
+							if valorFit == "bf" {
+									valorFit = "b"
+							} else if valorFit == "ff" {
+									valorFit = "f"
+							} else if valorFit == "wf" {
+									valorFit = "w"
+							}
+					}
 			}
-		}
-		if unit {
-			if valorUnit != "k" && valorUnit != "m" {
-				fmt.Println("Error: Unit no reconocido")
-				respuesta += "Error: Unit no reconocido\n"
-				return respuesta
+			if unit {
+					if valorUnit != "k" && valorUnit != "m" {
+							fmt.Println("Error: Unit no reconocido")
+							respuesta += "Error: Unit no reconocido\n"
+							return respuesta
+					}
 			}
-		}
-		//Pasar a int el size
-		sizeInt, err := strconv.Atoi(valorSize)
-		if err != nil {
-			fmt.Println("Error: Size no es un numero")
-			respuesta += "Error: Size no es un numero\n"
+			// Pasar a int el size
+			sizeInt, err := strconv.Atoi(valorSize)
+			if err != nil {
+					fmt.Println("Error: Size no es un número")
+					respuesta += "Error: Size no es un número\n"
+					return respuesta
+			}
+			// Verificar que el size sea mayor a 0
+			if sizeInt <= 0 {
+					fmt.Println("Error: Size debe ser mayor a 0")
+					respuesta += "Error: Size debe ser mayor a 0\n"
+					return respuesta
+			}
+			// Imprimir los valores
+			fmt.Println("Size: " + valorSize)
+			fmt.Println("Unit: " + valorUnit)
+			fmt.Println("Fit: " + valorFit)
+			fmt.Println("Path: " + valorPath)
+			// Llamar a la función para crear el disco
+			respuesta += CrearDiscos(sizeInt, valorUnit, valorFit, valorPath)
 			return respuesta
-		}
-		//Verificar que el size sea mayor a 0
-		if sizeInt <= 0 {
-			fmt.Println("Error: Size debe ser mayor a 0")
-			respuesta += "Error: Size debe ser mayor a 0\n"
-			return respuesta
-		}
-		//Imprimir los valores
-		fmt.Println("Size: " + valorSize)
-		fmt.Println("Unit: " + valorUnit)
-		fmt.Println("Fit: " + valorFit)
-		fmt.Println("Path: " + valorPath)
-		//Llamar a la funcion para crear el disco
-		respuesta += CrearDiscos(sizeInt, valorUnit, valorFit, valorPath)
-		return respuesta
 	}
 }
 
